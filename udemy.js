fc491a430a05e6dc2bb9149171c7080cc5601e68
Sim0n-sk8 <simonswart91@gmail.com>
2025-08-15 23:18:36 +0200

    .

---
+++ udemy.js
@@ -0,0 +1,11 @@
+function lifeInWeeks(age){
+    var cAge =21;
+    var endAge = 90;
+    var eAged = endAge *365;
+    var cAged = cAge * 365;
+
+    dLeft = eAged - cAged;
+
+    console.log(dLeft);
+
+}
