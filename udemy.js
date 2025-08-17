18e9bdf5f4e9d816f79b9ddbb9c1e5395a3544b6
Sim0n-sk8 <simonswart91@gmail.com>
2025-08-17 17:34:32 +0200

    Update udemy.js

--- udemy.js
+++ udemy.js
@@ -2,11 +2,28 @@ var output = [];
 var num = 1;
 function fizzbuzz(){
     
-    
-    output.push(num);
-    num++;
-    // works lol
+    fizz = 3;
+    buzz = 5;
 
+    max = 100;
+    
+    
+    
+    // works lol
+    
+    if(num % fizz === 0 && num % buzz === 0){
+        output.push("fizzbuzz");
+    } else if(num % buzz === 0){
+        output.push("buzz")
+    }else if(num % fizz === 0){
+        output.push("fizz");
+    }else{
+        output.push(num);
+        
+    }
+        num++;
+    
+    
 
     console.log(output);
 }
