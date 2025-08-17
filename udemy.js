df97755bff2be059bb891192d581a58e9c6de3e6
Sim0n-sk8 <simonswart91@gmail.com>
2025-08-17 18:25:10 +0200

    Update udemy.js

--- udemy.js
+++ udemy.js
@@ -1,20 +1,17 @@
-function whosPaying(names) {
+function beer(){
     
-/******Don't change the code above*******/
+    let beerLeft = 99;
+
+
+    while(beerLeft > 1){
+        console.log(beerLeft + " bottles of beer on the wall, " + beerLeft + "bottles of beer.");
+        beerLeft--;
+        console.log("Take one down and pass it around, " + beerLeft + " bottles of beer on the wall.")
+        
+    }
+
+    console.log("No more bottles of beer on the wall, no more bottles of beer.");
+    console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
     
-    //Write your code here.
-    
-
-
-
-    //gonna restart laptop heat issue
-    
-
-
-   //["Angela", "Ben", "Jenny", "Michael", "Chloe"]
-    
-    //Michael is going to buy lunch today!
-
-
-/******Don't change the code below*******/    
 }
+beer();
