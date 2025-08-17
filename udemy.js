aba7af383ef27aab7374ec1c3d2206dfb4e3c37e
Sim0n-sk8 <simonswart91@gmail.com>
2025-08-17 15:41:05 +0200

    Update udemy.js

--- udemy.js
+++ udemy.js
@@ -2,21 +2,20 @@ function isLeap(year) {
     
 /**************Don't change the code above****************/    
     
- 
     if( year % 400 === 0){
          alert("Leap year.");
     }
-     if (year % 100 === 0){
+     else if (year % 100 === 0){
         alert("Not leap year.");
     }
 
-    if( year % 4 === 0){
+    else if( year % 4 === 0){
          alert("Leap year.");
     }
 
-
-
-    
+    else{
+         alert("Not leap year.");
+    }
 
 /**************Don't change the code below****************/    
 
