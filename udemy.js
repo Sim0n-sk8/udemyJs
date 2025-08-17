91b6f4401e829365425df6607863e2b4c4ecc3b1
Sim0n-sk8 <simonswart91@gmail.com>
2025-08-17 15:31:16 +0200

    Update udemy.js

--- udemy.js
+++ udemy.js
@@ -1,6 +1,19 @@
-function bmiCalculator (weight, height) {
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+    
+   //if divide by 4 === true then yes
+    if( year % 4 === 0 && year % 100 !== 0 && year % 400 === 0){
+        alert(`${year} is a leap year`);
+    }else{
+        alert(`${year} is not a leap year`);
+    }
+   //if divide by 4 === true and divide by 100 === true then no
+   //if divide by 4 === true and divide by 100 === true and divide by 400 === true then yes
 
-    prompt 
 
-    return interpretation;
+    
+
+/**************Don't change the code below****************/    
+
 }
