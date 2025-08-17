ad0cb31a0c061ecc729f0c72ae052e4d1a32ea8f
Sim0n-sk8 <simonswart91@gmail.com>
2025-08-17 15:37:03 +0200

    Update udemy.js

--- udemy.js
+++ udemy.js
@@ -2,14 +2,18 @@ function isLeap(year) {
     
 /**************Don't change the code above****************/    
     
-   //if divide by 4 === true then yes
-    if( year % 4 === 0 && year % 100 !== 0 && year % 400 === 0){
-        alert(`${year} is a leap year`);
-    }else{
-        alert(`${year} is not a leap year`);
+ 
+    if( year % 400 === 0){
+         alert("Leap year.");
     }
-   //if divide by 4 === true and divide by 100 === true then no
-   //if divide by 4 === true and divide by 100 === true and divide by 400 === true then yes
+     if (year % 100 === 0){
+        alert("Not leap year.");
+    }
+
+    if( year % 4 === 0){
+         alert("Leap year.");
+    }
+
 
 
     
